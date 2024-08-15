bf84ef5235e11e61b9b1a0b6879ba63cbcb99e66
Walter Isaac <walter4real40@gmail.com>
2024-08-15 16:19:53 +0100

    updated

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import Book
-
+from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from .models import Library
 
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required, user_passes_test
 
 
@@ -34,22 +34,13 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
 
-def login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            auth_login(request, user)
-            return redirect('home') # NEEDS WORK
-    else:
-        form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
+class LoginView(LoginView):
+    template_name = 'registration/login.html'
+    success_url = reverse_lazy('home')
+    redirect_authenticated_user = True
 
-def logout(request):
-    if request.method == 'POST':
-        auth_logout(request)
-        return redirect('logout_done')
-    return render(request, 'logout.html')
+class LogoutView(LogoutView):
+    next_page = reverse_lazy('home')
 
 @login_required
 def home(request):
