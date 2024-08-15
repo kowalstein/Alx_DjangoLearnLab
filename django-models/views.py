80f6bf595d7c0dd0416898965c667f5ee83cdc11
Walter Isaac <walter4real40@gmail.com>
2024-08-15 14:50:14 +0100

    up

---
+++ django-models/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from .models import Book
+
+from django.views.generic.detail import DetailView
+from .models import Library
+
+from django.contrib.auth.decorators import user_passes_test
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()  # Assuming the related_name for books in Library model is 'books'
+        return context
+
+from django.contrib.auth.decorators import user_passes_test
+
+@user_passes_test(lambda u: u.userprofile.role == 'Admin')
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+@user_passes_test(lambda u: u.userprofile.role == 'Librarian')
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@user_passes_test(lambda u: u.userprofile.role == 'Member')
+def member_view(request):
+    return render(request, 'member_view.html')
+# Create your views here.
