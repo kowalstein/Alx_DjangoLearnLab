336007a47383e3f349d7809b057fbb67f24edc6d
Walter Isaac <walter4real40@gmail.com>
2024-08-16 15:31:11 +0100

    upgrade

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -49,13 +49,13 @@ def home(request):
 
 @user_passes_test(lambda u: u.userprofile.role == 'Admin')
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(lambda u: u.userprofile.role == 'Librarian')
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
 @user_passes_test(lambda u: u.userprofile.role == 'Member')
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'relationship_app/member_view.html')
 # Create your views here.
