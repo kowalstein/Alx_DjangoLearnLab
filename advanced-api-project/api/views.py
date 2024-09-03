dacdafe0e09f7b339a41b34ed7248126461882d8
Walter Isaac <walter4real40@gmail.com>
2024-09-03 12:04:59 +0100

    added custom views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Book
+from .seriealizers import Book
+from .seriealizers import BookSerializer
+
 
 # Create your views here.
+
+class BookListView(generics.ListCreateAPIView):
+    """
+    Retrieves a list of all books or create a new book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Custom actions before saving instance
+        serializer.save()
+
+
+
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update, or delete a specific book by ID.   
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_update(self, serializer):
+        # Custom actions before updating instance
+        serializer.save()
+
