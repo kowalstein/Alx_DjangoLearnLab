27bc907268bcdd12cc3666df9020ae07ae9ace7a
Walter Isaac <walter4real40@gmail.com>
2024-09-12 14:53:05 +0100

    user authentication added

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,44 +1,44 @@
+# blog/views.py
+
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserChangeForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('profile')
+            return redirect('/')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'blog/register.html', {'form': form})
 
 def user_login(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            login(request, form.get_user())
-            if 'next' in request.POST:
-                return redirect(request.POST.get('next'))
-            else:
-                return redirect("profile")
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
     else:
         form = AuthenticationForm()
-    return render(request, 'users/login.html', { "form": form })
+    return render(request, 'blog/login.html', {'form': form})
 
 def user_logout(request):
     logout(request)
-    return redirect('login')
+    return redirect('/')
 
 @login_required
 def profile(request):
     if request.method == 'POST':
-        form = UserChangeForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect('profile')
-    else:
-        form = UserChangeForm(instance=request.user)
-    return render(request, 'blog/profile.html', {'form': form})
+        user = request.user
+        user.email = request.POST.get('email')
+        user.save()
+        return redirect('profile')
+    return render(request, 'blog/profile.html')
+
