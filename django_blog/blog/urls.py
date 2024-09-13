d7df37fef9ddfacaa78acd86209a580d843218fc
Walter Isaac <walter4real40@gmail.com>
2024-09-13 12:37:31 +0100

    added coment functionality

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view, name='post-delete'),
     path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-edit'),
-    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]
